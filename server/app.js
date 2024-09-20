a40351fe60edf6d508ddf21bb4b10d1b4cb28eda
Om Kokate <om.kokate2902@gmail.com>
2024-09-20 13:31:32 +0530

    server auth done

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const { protect } = require("./middlewares/authMiddleware");
+const cors = require("cors"); // Import the CORS package
+
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3001", // Allow requests from this origin
+    credentials: true, // Allow cookies to be sent
+  })
+);
+
+// Connect to MongoDB
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Testing Branch Works fine");
+});
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

7f3911d83960d12c3ea0681ab3946bbefce48ce7
Asmer Chougle <asmer72582@gmail.com>
2024-09-20 20:55:14 +0530

    Header added

--- client/src/pages/CreateTest.js
+++ client/src/pages/CreateTest.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Header from '../components/Header';
 
 export default function CreateTest() {
     const [questions, setQuestions] = useState([
@@ -63,101 +64,7 @@ export default function CreateTest() {
     return (
         <>
             <div className='app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header closed-sidebar'>
-                <div className="app-header header-shadow">
-                    <div className="app-header__logo">
-                        <div className="logo-src"></div>
-                        <div className="header__pane ml-auto">
-                            <div><a href="/home">logo</a></div>
-                        </div>
-                    </div>
-                    <div className="app-header__menu">
-                        <span>
-                            <button type="button" className="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
-                                <span className="btn-icon-wrapper">
-                                    <i className="fa fa-ellipsis-v fa-w-6"></i>
-                                </span>
-                            </button>
-                        </span>
-                    </div>
-                    <div class="app-header__menu">
-                        <span>
-                            <button type="button"
-                                class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
-                                <span class="btn-icon-wrapper">
-                                    <i class="fa fa-ellipsis-v fa-w-6"></i>
-                                </span>
-                            </button>
-                        </span>
-                    </div>
-                    <div class="app-header__content">
-                        <div class="app-header-left">
-                            <div class="search-wrapper">
-                                <div class="input-holder">
-                                    <input type="text" class="search-input" placeholder="Type to search" />
-                                    <button class="search-icon"><span></span></button>
-                                </div>
-                                <button class="close"></button>
-                            </div>
-                            <ul class="header-menu nav">
-                                <li class="nav-item">
-                                    <a href="javascript:void(0);" class="nav-link">
-                                        <i class="nav-link-icon fa fa-book"> </i>
-                                        My Tests
-                                    </a>
-                                </li>
-
-
-                                <li class="dropdown nav-item">
-                                    <a href="javascript:void(0);" class="nav-link">
-                                        <i class="nav-link-icon fa fa-archive"></i>
-                                        Explore Tests
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="app-header-right">
-                            <div class="header-btn-lg pr-0">
-                                <div class="widget-content p-0">
-                                    <div class="widget-content-wrapper">
-                                        <div class="widget-content-left">
-                                            <div class="btn-group">
-
-
-                                                <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-                                                    class="p-0 btn">
-                                                    <img width="42" class="rounded-circle" src="assets/images/avatars/1.jpg"
-                                                        alt="" />
-                                                    <i class="fa fa-angle-down ml-2 opacity-8"></i>
-                                                </a>
-                                                <div tabindex="-1" role="menu" aria-hidden="true"
-                                                    class="dropdown-menu dropdown-menu-right">
-                                                    <button type="button" tabindex="0" class="dropdown-item">User
-                                                        Account</button>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="widget-content-left  ml-3 header-user-info">
-                                            <div class="widget-heading">
-                                                Username
-                                            </div>
-                                            <div class="widget-subheading">
-                                                Organisation
-                                            </div>
-                                        </div>
-                                        <div class="widget-content-right header-user-info ml-3">
-                                            <button type="button"
-                                                class="btn-shadow p-1 btn btn-primary btn-sm show-toastr-example">
-                                                <i class="fa text-white fa-user pr-1 pl-1"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
+                <Header />
                 <div className="app-main">
                     <div className="app-main__outer container">
                         <div className="app-main__inner container">

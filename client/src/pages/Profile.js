287cb2aa588e61e5a4e71a2e7a3868e6c04f570b
Asmer Chougle <asmer72582@gmail.com>
2024-09-21 00:39:19 +0530

    profile Done

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./profile.css";
+import Header from "../components/Header";
 
 function Profile({ token, userId }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -63,43 +64,103 @@ function Profile({ token, userId }) {
 
   return (
     <div>
-      <button onClick={handleVerifyClick}>Verify Yourself</button>
 
-      {isModalOpen && (
-        <div className="modal">
-          <div className="modal-content">
-            <h2>Upload Your Image</h2>
+      <div className="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header closed-sidebar">
+        <Header />
+        <br /><br />
+        <br /><br />
 
-            <input
-              type="file"
-              accept="image/*"
-              onChange={handleImageChange}
-              style={{ margin: "10px 0" }}
-            />
+        <div class="container-fluid">
+          <div class="row profile">
+            <div class="col-md-3">
+              <div class="profile-sidebar">
 
-            {previewImage && (
-              <div>
-                <img
-                  src={previewImage}
-                  alt="Selected"
-                  style={{
-                    width: "100%",
-                    maxHeight: "300px",
-                    objectFit: "contain",
-                  }}
-                />
+                <div class="profile-userpic ">
+                  <img src="https://gravatar.com/avatar/31b64e4876d603ce78e04102c67d6144?s=80&d=https://codepen.io/assets/avatars/user-avatar-80x80-bdcd44a3bfb9a5fd01eb8b86f9e033fa1a9897c3a15b33adfc2649a002dab1b6.png" class="img-responsive" alt="" />
+                </div>
+
+                <div class="profile-usertitle">
+                  <div class="profile-usertitle-name">
+                    UserName
+                  </div>
+                  <div class="profile-usertitle-job">
+                    Organisation
+                  </div>
+                </div>
+
+                <div class="profile-userbuttons">
+
+                  <button type="button" onClick={handleVerifyClick} class="btn btn-danger btn-sm">Verify Photo</button>
+                </div>
+
+                <br />
+
+
+
+                <div class="portlet light bordered">
+
+                  <div class="row list-separated profile-stat">
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                      <div class="uppercase profile-stat-title"> 37 </div>
+                      <div class="uppercase profile-stat-text"> Test Attempts </div>
+                    </div>
+
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                      <div class="uppercase profile-stat-title"> 6 </div>
+                      <div class="uppercase profile-stat-text"> Test Marked </div>
+                    </div>
+                  </div>
+
+
+                </div>
+
               </div>
-            )}
-
-            <div style={{ marginTop: "20px" }}>
-              <button onClick={handleUpload} style={{ marginRight: "10px" }}>
-                Upload
-              </button>
-              <button onClick={handleCloseModal}>Cancel</button>
+            </div>
+            <div class="col-md-9">
+              <div class="profile-content">
+                Some user related content goes here...
+              </div>
             </div>
           </div>
         </div>
-      )}
+        {/* <button onClick={handleVerifyClick}>Verify Yourself</button> */}
+
+        {isModalOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <h2>Upload Your Image</h2>
+
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+                style={{ margin: "10px 0" }}
+              />
+
+              {previewImage && (
+                <div>
+                  <img
+                    src={previewImage}
+                    alt="Selected"
+                    style={{
+                      width: "100%",
+                      maxHeight: "300px",
+                      objectFit: "contain",
+                    }}
+                  />
+                </div>
+              )}
+
+              <div style={{ marginTop: "20px" }}>
+                <button onClick={handleUpload} style={{ marginRight: "10px" }}>
+                  Upload
+                </button>
+                <button onClick={handleCloseModal}>Cancel</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
